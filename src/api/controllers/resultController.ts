5af1098ecf2586938a73f1fa8a7e2812b5b4ae66
TomiHenriksson8 <tomihen01@gmail.com>
2024-10-07 20:03:21 +0300

    Add controllers and routes for comparing quiz stats

--- src/api/controllers/resultController.ts
+++ src/api/controllers/resultController.ts
@@ -5,7 +5,11 @@ import ResultModel from "../models/resultModel";
 import { PopulatedQuiz } from "../../types/quizTypes";
 
 export const submitQuizResult = async (
-  req: Request<{}, {}, { quizId: string; answers: { questionId: string; answerId: string }[] }>,
+  req: Request<
+    {},
+    {},
+    { quizId: string; answers: { questionId: string; answerId: string }[] }
+  >,
   res: Response,
   next: NextFunction
 ) => {
@@ -27,7 +31,10 @@ export const submitQuizResult = async (
         (a) => a.questionId === question._id.toString()
       );
       const correctAnswer = question.answers.find((a: any) => a.isCorrect);
-      if (correctAnswer && correctAnswer._id.toString() === userAnswer?.answerId) {
+      if (
+        correctAnswer &&
+        correctAnswer._id.toString() === userAnswer?.answerId
+      ) {
         correctAnswers++;
       }
     });
@@ -62,4 +69,48 @@ export const submitQuizResult = async (
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
   }
+};
+
+export const compareQuizResult = async (
+  req: Request<{ quizId: string}, {}, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { quizId } = req.params
+  const userId = (req as any).user?.id;
+
+  try {
+
+    const quiz = await QuizModel.findById(quizId)
+    if (!quiz) {
+      return next(new CustomError("Quiz not found", 404))
+    }
+
+    const allResults = await ResultModel.find({ quizId })
+    if (allResults.length === 0) {
+      return res.status(404).json({ message: "No results found for this quiz" })
+    }
+
+    const userResult = await ResultModel.findOne({ userId, quizId })
+    if (!userResult) {
+      return res.status(404).json({message: "User has not taken this quiz yet."})
+    }
+
+    const betterThanCount = allResults.filter(
+      (result) => result.correctAnswers < userResult.correctAnswers
+    ).length
+
+    const percentage = allResults.length > 1 ? (betterThanCount / (allResults.length - 1)) * 100 : 100;
+
+
+    res.status(200).json({
+      correctAnswers: userResult.correctAnswers,
+      totalQuestions: userResult.totalQuestions,
+      betterThanCount,
+      totalUsers: allResults.length,
+      percentage
+    })
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+ }
 };
--- src/api/routes/resultRoutes.ts
+++ src/api/routes/resultRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { submitQuizResult } from "../controllers/resultController";
+import { compareQuizResult, submitQuizResult } from "../controllers/resultController";
 import { protect } from "../../middlewares";
 
 const router = Router();
 
+router.get("/compare/:quizId", protect, compareQuizResult);
 router.post('/submit-quiz', protect, submitQuizResult)
 
 export default router
